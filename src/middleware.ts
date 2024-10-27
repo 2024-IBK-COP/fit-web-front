85a33db584dc35a7901fe2ed150835256eaf350d
우경이 <radiata03@gmail.com>
2024-10-28 01:50:19 +0900

    d

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,6 @@ export { default } from "next-auth/middleware"
 
 export const config = { 
     matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico|login).*)',
+        '/((?!api|_next/static|_next/image|_next/img|favicon.ico|login).*)',
     ] 
 }
