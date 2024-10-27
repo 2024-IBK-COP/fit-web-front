00961bb0b4d8895b3f5454a57db7a2b3cedef97c
우경이 <radiata03@gmail.com>
2024-10-28 01:39:54 +0900

    D

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // distDir: 'build',
+    // output: 'export',
     // reactStrictMode: true,
     // swcMinify: true,
     // async rewrites() {
